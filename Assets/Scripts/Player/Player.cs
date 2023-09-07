c22cfbae25054c7771ba35acad509ca7608beeb0
yankaique <yan.contato.dev@gmail.com>
2023-09-07 10:25:56 -0300

    feat: add jump to character and ant-fric in grounds

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigibody;
-    public Vector2 velocity;
     public float speed;
+    public float forceJump = 2;
+    public Vector2 friction = new Vector2(.1f, 0);
+
+    // public Vector2 velocity;
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) {
+        HandleMovement();
+        HandleJump();
+    }
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
             // myRigibody.MovePosition(myRigibody.position - velocity * Time.deltaTime);
-            myRigibody.velocity = new Vector2 (-speed, myRigibody.velocity.y);
-        }else if (Input.GetKey(KeyCode.RightArrow))
+            myRigibody.velocity = new Vector2(-speed, myRigibody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigibody.MovePosition(myRigibody.position + velocity * Time.deltaTime);
             myRigibody.velocity = new Vector2(speed, myRigibody.velocity.y);
         }
+
+        if (myRigibody.velocity.x > 0)
+        {
+            myRigibody.velocity -= friction;
+        }
+        else if (myRigibody.velocity.x < 0)
+        {
+            myRigibody.velocity += friction;
+        }
     }
+
+        private void HandleJump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // myRigibody.MovePosition(myRigibody.position - velocity * Time.deltaTime);
+                myRigibody.velocity = Vector2.up * forceJump;
+            }
+        }
 }
