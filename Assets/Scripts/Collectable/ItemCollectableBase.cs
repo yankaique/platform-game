8c55ad63b79fc59e104b4be65c759137bcf36add
yankaique <yan.contato.dev@gmail.com>
2023-09-10 10:17:11 -0300

    feat: created coin system

---
+++ Assets/Scripts/Collectable/ItemCollectableBase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectableBase : MonoBehaviour
+{
+    public string compareTag = "Player";
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     
+        if (collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+        
+    }
+    protected virtual void Collect()
+    {
+        Debug.Log("Encostou na moeda");
+        gameObject.SetActive(false);
+        OnCollect();
+    }
+
+    protected virtual void OnCollect()
+    {
+
+    }
+}
